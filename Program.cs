15649984b803b714036418b81eea3171016f9d51
Erling Paulsen <erling.paulsen@aenergi.no>
2024-11-20 22:28:09 +0100

    Minimal repro for issue

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Npgsql;
+
+Console.WriteLine("Hello, World!");
+
+
+
+
+
+var connection = new NpgsqlConnection(args[0]);
+
+await connection.OpenAsync();
+var command = connection.CreateCommand();
+command.CommandText = "SELECT 1";
+await command.ExecuteNonQueryAsync();
+
+await connection.CloseAsync();
+
+Console.WriteLine("Success if we got here!");
